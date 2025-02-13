a92529c3205d7b7c36411556dc02461c2dafd241
admin <admin@adminui-MacBookPro.local>
2025-02-13 11:09:48 +0900

    feat: Local transform 코드 구현
    1. 텍스트 문장 분리
    2. 불필요한 토큰을 줄이기 위한 텍스트 Cleansing
    
    Resolves: #9

--- src/etl.py
+++ src/etl.py
@@ -4,15 +4,25 @@ from extract.bobae import extract_bobae
 from extract.youtube import extract_youtube
 from extract.clien import extract_clien
 
+from transform.transform_text import transform_text
+
+def extract(input_date, car_name):
+    print("Extract started")
+    extract_youtube(input_date, car_name)
+    extract_bobae(input_date, car_name)
+    extract_clien(input_date, car_name)
+
+def transform(input_date, car_name):
+    print("Transform started")
+    transform_text(input_date, car_name)
 
 def etl(input_date, car_name):
     print("ETL started")
-    extract_bobae(input_date, car_name)
-    extract_youtube(input_date, car_name)
-    extract_clien(input_date, car_name)
+    # extract(input_date, car_name)
 
     # transform here
-
+    transform_text(input_date, car_name)
+    
     print("ETL done")
 
 
