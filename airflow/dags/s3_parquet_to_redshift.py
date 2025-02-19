05d474a37d6efd63786f8b0423bcfe1185137c1e
openkmj <openkmj@gmail.com>
2025-02-19 12:06:26 +0900

    fix: Fix import error

--- airflow/dags/s3_parquet_to_redshift.py
+++ airflow/dags/s3_parquet_to_redshift.py
@@ -1,4 +1,3 @@
-
 from airflow import DAG
 from airflow.models import Variable
 from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
@@ -12,23 +11,25 @@ from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOp
 task_params = [
     {"TABLE_NAME": "posts_tb", "S3_KEY": "post_data/"},
     {"TABLE_NAME": "comments_tb", "S3_KEY": "comment_data/"},
-    {"TABLE_NAME": "sentence_tb", "S3_KEY": "sentence_data/"}
+    {"TABLE_NAME": "sentence_tb", "S3_KEY": "sentence_data/"},
 ]
 car_names = Variable.get("car_name", deserialize_json=True)
 S3_BUCKET = "the-all-new-bucket"
 
 with DAG(
     dag_id="s3_to_redshift",
-    #default_args=default_args,
+    # default_args=default_args,
     # schedule_interval=timedelta(days=1),
-    description="Copy data from s3 to Redshift"
+    description="Copy data from s3 to Redshift",
 ) as dag:
     for car_name in car_names:
         tasks = []
         for params in task_params:
-            s3_object = s3_object = f"{car_name}/{{ data_interval_start.strftime('%Y') }}/{{ data_interval_start.strftime('%m') }}/{params['S3_KEY']}"
+            s3_object = s3_object = (
+                f"{car_name}/{{ data_interval_start.strftime('%Y') }}/{{ data_interval_start.strftime('%m') }}/{params['S3_KEY']}"
+            )
             task = S3ToRedshiftOperator(
-                task_id=f"copy_data_from_s3_{params['TABLE_NAME']}",
+                task_id=f"copy_data_from_s3_{car_name}_{params['TABLE_NAME']}",
                 schema="public",
                 table=params["TABLE_NAME"],
                 s3_bucket=S3_BUCKET,
