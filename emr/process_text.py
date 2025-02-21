9876e47b229af397ad6b3bcfb91e7e9b195c22f6
openkmj <openkmj@gmail.com>
2025-02-21 11:24:19 +0900

    feat: Rename column names and add comment_id

--- emr/process_text.py
+++ emr/process_text.py
@@ -8,7 +8,8 @@ from pyspark.sql.types import StringType
 
 BUCKET_NAME = "the-all-new-bucket"
 
-def get_timestamp(year,month,day):
+
+def get_timestamp(year, month, day):
     # 한국 시간(KST) 기준의 타임존 설정
     KST = timezone(timedelta(hours=9))
     year = int(year)
@@ -23,25 +24,30 @@ def get_timestamp(year,month,day):
     start_timestamp = int(start_dt.timestamp())  # 초 단위 정수 변환
     end_timestamp = int(end_dt.timestamp())
 
-    return start_timestamp, end_timestamp    
+    return start_timestamp, end_timestamp
+
 
 def save_missing_post_data(df, data_type, car_name, year, month, day):
     """
     결측치가 있는 데이터를 별도로 저장하는 함수
     """
-    # 결측치가 있는 데이터를 필터링 
+    # 결측치가 있는 데이터를 필터링
     missing_data_df = df.filter(
-        col("id").isNull() |
-        col("car_name").isNull() |
-        col("source").isNull() |
-        col("title").isNull() | 
-        col("nickname").isNull() | 
-        col("article").isNull() |
-        col("view_count").isNull() | (col("view_count") < 0) |
-        col("like_count").isNull() | (col("like_count") < 0) |
-        col("dislike_count").isNull() |  (col("dislike_count") < 0) |
-        col("date").isNull() | 
-        col("comment_count").isNull() | (col("comment_count") < 0) 
+        col("id").isNull()
+        | col("car_name").isNull()
+        | col("source").isNull()
+        | col("title").isNull()
+        | col("nickname").isNull()
+        | col("article").isNull()
+        | col("view_count").isNull()
+        | (col("view_count") < 0)
+        | col("like_count").isNull()
+        | (col("like_count") < 0)
+        | col("dislike_count").isNull()
+        | (col("dislike_count") < 0)
+        | col("date").isNull()
+        | col("comment_count").isNull()
+        | (col("comment_count") < 0)
     )
 
     if missing_data_df.count() > 0:
@@ -52,23 +58,24 @@ def save_missing_post_data(df, data_type, car_name, year, month, day):
         print(f"Missing data for {data_type} saved.")
     else:
         print(f"No missing data for {data_type}.")
-    
+
     # 결측치가 있는 데이터를 제외한 DataFrame 반환
     df_cleaned = df.filter(
-        col("id").isNotNull() &
-        col("car_name").isNotNull() &
-        col("source").isNotNull() &
-        col("title").isNotNull() &
-        col("nickname").isNotNull() & 
-        col("article").isNotNull() &
-        col("view_count").isNotNull() &
-        col("like_count").isNotNull() &
-        col("dislike_count").isNotNull() &
-        col("date").isNotNull() &
-        col("comment_count").isNotNull()
+        col("id").isNotNull()
+        & col("car_name").isNotNull()
+        & col("source").isNotNull()
+        & col("title").isNotNull()
+        & col("nickname").isNotNull()
+        & col("article").isNotNull()
+        & col("view_count").isNotNull()
+        & col("like_count").isNotNull()
+        & col("dislike_count").isNotNull()
+        & col("date").isNotNull()
+        & col("comment_count").isNotNull()
     )
 
     return df_cleaned
+
 
 def save_missing_comment_data(df, data_type, car_name, year, month, day):
     """
@@ -76,12 +83,14 @@ def save_missing_comment_data(df, data_type, car_name, year, month, day):
     """
     # 결측치가 있는 데이터를 필터링 (timestamp, title 등에서 null 체크)
     missing_data_df = df.filter(
-        col("comment_uuid").isNull() |
-        col("author").isNull() |
-        col("content").isNull() |
-        col("timestamp").isNull() | 
-        col("like_cnt").isNull() | (col("like_cnt") < 0) |
-        col("dislike_cnt").isNull() |(col("dislike_cnt") < 0) 
+        col("comment_uuid").isNull()
+        | col("author").isNull()
+        | col("content").isNull()
+        | col("timestamp").isNull()
+        | col("like_cnt").isNull()
+        | (col("like_cnt") < 0)
+        | col("dislike_cnt").isNull()
+        | (col("dislike_cnt") < 0)
     )
 
     if missing_data_df.count() > 0:
@@ -92,52 +101,58 @@ def save_missing_comment_data(df, data_type, car_name, year, month, day):
         print(f"Missing data for {data_type} saved.")
     else:
         print(f"No missing data for {data_type}.")
-    
+
     # 결측치가 있는 데이터를 제외한 DataFrame 반환
     df_cleaned = df.filter(
-        col("comment_uuid").isNotNull() & 
-        col("author").isNotNull() & 
-        col("content").isNotNull() & 
-        col("timestamp").isNotNull() & 
-        col("like_cnt").isNotNull() & 
-        col("dislike_cnt").isNotNull()
+        col("comment_uuid").isNotNull()
+        & col("author").isNotNull()
+        & col("content").isNotNull()
+        & col("timestamp").isNotNull()
+        & col("like_cnt").isNotNull()
+        & col("dislike_cnt").isNotNull()
     )
 
     return df_cleaned
 
 
+def seperate_post_and_comment(df, source, car_name, year, month, day):
 
-def seperate_post_and_comment(df, source, car_name,year,month,day):
-    
-    #post data의 결측치&이상치 따로 저장
+    # post data의 결측치&이상치 따로 저장
     df = save_missing_post_data(df, source, car_name, year, month, day)
-    
+
     # `uuid()`로 `post_uuid` 생성
     post_df = df.withColumn("post_uuid", F.expr("uuid()"))
 
     # explode() 적용 전에 comment_count > 0 필터링
     comment_df = post_df.filter(F.col("comment_count") > 0)
 
-    # explode() 적용
+    # explode() 적용.
     comment_df = comment_df.withColumn("comment", F.explode("comments"))
 
     # rename columns
     comment_df = comment_df.select(
+        "post_uuid",
         F.expr("uuid()").alias("comment_uuid"),
+        F.concat(
+            F.col("id"),
+            F.lit("_"),
+            F.hash(
+                F.col("comment.comment_nickname"), F.col("comment.comment_content")
+            ).cast("string"),
+        ).alias("comment_id"),
         F.col("comment.comment_nickname").alias("author"),
         F.col("comment.comment_content").alias("content"),
-        F.col("comment.comment_date").alias("timestamp"),
+        F.col("comment.comment_date").alias("create_timestamp"),
         F.col("comment.comment_like_count").alias("like_cnt"),
         F.col("comment.comment_dislike_count").alias("dislike_cnt"),
-        "post_uuid",
     )
-    
+
     # comment data의 결측치&이상치 따로 저장
-    comment_df = save_missing_comment_data(comment_df, source, car_name, year, month, day)
-    
+    comment_df = save_missing_comment_data(
+        comment_df, source, car_name, year, month, day
+    )
+
     post_df = post_df.drop("comments")
-    #post_df = post_df.withColumn("car_name", F.lit(car_name))
-    #post_df = post_df.withColumn("source", F.lit(source))
 
     # rename columns
     post_df = post_df.select(
@@ -146,31 +161,31 @@ def seperate_post_and_comment(df, source, car_name,year,month,day):
         "title",
         F.col("nickname").alias("author"),
         "article",
-        F.col("date").alias("timestamp"),
+        F.col("date").alias("create_timestamp"),
         F.col("like_count").alias("like_cnt"),
         F.col("dislike_count").alias("dislike_cnt"),
         F.col("view_count").alias("view_cnt"),
         F.col("comment_count").alias("comment_cnt"),
+        "car_name",
         "source",
-        "car_name"
     )
     return post_df, comment_df
 
 
 def explode_post(post_df):
     title_sentence_df = post_df.select(
-        F.expr("uuid()").alias("sentence_uuid"),
-        col("post_uuid"),
-        lit(None).cast("string").alias("comment_id"),
-        lit("post").alias("type"),
-        col("title").alias("text"),
-    )
-    article_sentence_df = post_df.select(
-        F.expr("uuid()").alias("sentence_id"),
         col("post_uuid"),
         lit(None).alias("comment_uuid"),
+        F.expr("uuid()").alias("sentence_uuid"),
         lit("post").alias("type"),
-        explode(split(col("article"), "\n")).alias("text"),
+        col("title").alias("sentence"),
+    )
+    article_sentence_df = post_df.select(
+        col("post_uuid"),
+        lit(None).alias("comment_uuid"),
+        F.expr("uuid()").alias("sentence_uuid"),
+        lit("post").alias("type"),
+        explode(split(col("article"), "\n")).alias("sentence"),
     )
 
     sentence_df = title_sentence_df.union(article_sentence_df)
@@ -180,35 +195,37 @@ def explode_post(post_df):
 
 def explode_comment(comment_df):
     sentence_df = comment_df.select(
-        F.expr("uuid()").alias("sentence_uuid"),
         col("post_uuid"),
         col("comment_uuid"),
+        F.expr("uuid()").alias("sentence_uuid"),
         lit("comment").alias("type"),
-        col("content").alias("text"),
+        col("content").alias("sentence"),
     )
     return sentence_df
 
 
-def clean_text(df):
-    df = df.filter(length(col("text")) > 10)
-    df = df.filter(col("text").rlike(".*[가-힣].*"))
-    df = df.withColumn("text", lower(col("text")))
-    df = df.withColumn("text", regexp_replace(col("text"), r"http\S+", ""))
-    df = df.withColumn("text", regexp_replace(col("text"), r"https\S+", ""))
-    df = df.withColumn("text", regexp_replace(col("text"), r"@\S+", ""))
-    df = df.withColumn("text", regexp_replace(col("text"), r"#\S+", ""))
-    df = df.withColumn("text", regexp_replace("text", r'[",\[\]]+', ""))
-    df = df.withColumn("text", regexp_replace(col("text"), r"([.!?,~])\1+", "$1"))
+def clean_sentence(df):
+    df = df.filter(length(col("sentence")) > 10)
+    df = df.filter(col("sentence").rlike(".*[가-힣].*"))
+    df = df.withColumn("sentence", lower(col("sentence")))
+    df = df.withColumn("sentence", regexp_replace(col("sentence"), r"http\S+", ""))
+    df = df.withColumn("sentence", regexp_replace(col("sentence"), r"https\S+", ""))
+    df = df.withColumn("sentence", regexp_replace(col("sentence"), r"@\S+", ""))
+    df = df.withColumn("sentence", regexp_replace(col("sentence"), r"#\S+", ""))
+    df = df.withColumn("sentence", regexp_replace("sentence", r'[",\[\]]+', ""))
     df = df.withColumn(
-        "text",
+        "sentence", regexp_replace(col("sentence"), r"([.!?,~])\1+", "$1")
+    )
+    df = df.withColumn(
+        "sentence",
         regexp_replace(
-            col("text"),
+            col("sentence"),
             r"[\u200B-\u200F\u202A-\u202E\u2060-\u206F\uFEFF\u00A0]",
             "",
         ),
     )
-    df = df.withColumn("text", trim(regexp_replace(col("text"), r"\s+", " ")))
-    df = df.filter(length(col("text")) > 10)
+    df = df.withColumn("sentence", trim(regexp_replace(col("sentence"), r"\s+", " ")))
+    df = df.filter(length(col("sentence")) > 10)
     return df
 
 
@@ -231,15 +248,14 @@ def process_text(year, month, day, car_name):
 
     # raw 데이터를 post, comment로 분리하기
     youtube_post_df, youtube_comment_df = seperate_post_and_comment(
-        youtube_raw_df, "youtube", car_name, year,month,day
+        youtube_raw_df, "youtube", car_name, year, month, day
     )
     bobae_post_df, bobae_comment_df = seperate_post_and_comment(
-        bobae_raw_df, "bobae", car_name,year,month,day
+        bobae_raw_df, "bobae", car_name, year, month, day
     )
     clien_post_df, clien_comment_df = seperate_post_and_comment(
-        clien_raw_df, "clien", car_name,year,month,day
+        clien_raw_df, "clien", car_name, year, month, day
     )
-    
 
     # source 별 post, comment 합치기
     post_df = youtube_post_df.union(bobae_post_df).union(clien_post_df)
@@ -254,16 +270,15 @@ def process_text(year, month, day, car_name):
     )
 
     # 새로 생긴 데이터만 남기기
-    start_timestamp, end_timestamp = get_timestamp(year,month, day)
-    
+    start_timestamp, end_timestamp = get_timestamp(year, month, day)
+
     comment_df = comment_df.filter(
-        (start_timestamp<=col("timestamp"))& (col("timestamp")<=end_timestamp)
-    )   
+        (start_timestamp <= col("timestamp")) & (col("timestamp") <= end_timestamp)
+    )
     post_df = post_df.filter(
-        (start_timestamp<=col("timestamp"))& (col("timestamp")<=end_timestamp)
-    )   
-    
-    
+        (start_timestamp <= col("timestamp")) & (col("timestamp") <= end_timestamp)
+    )
+
     # sentence 추출
     post_sentence_df = explode_post(post_df)
     comment_sentence_df = explode_comment(comment_df)
@@ -271,7 +286,7 @@ def process_text(year, month, day, car_name):
     sentence_df = post_sentence_df.union(comment_sentence_df)
 
     # sentence data를 rule based 로 정제(특수 문자 제거 등)
-    sentence_df = clean_text(sentence_df)
+    sentence_df = clean_sentence(sentence_df)
 
     sentence_df.repartition(10).write.mode("overwrite").parquet(
         f"s3://{BUCKET_NAME}/{car_name}/{year}/{month}/{day}/sentence_data"
