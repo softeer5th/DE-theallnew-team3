ccb330163389d2aa6fa17936d0e45655cd6eef70
admin <admin@adminui-MacBookPro.local>
2025-02-24 11:57:17 +0900

    feat: Impl materialized view for anlaysis

--- airflow/dags/sql/analysis_view.sql
+++ airflow/dags/sql/analysis_view.sql
@@ -1,26 +1,2 @@
-CREATE MATERIALIZED VIEW social_listening AS
-    SELECT
-        k.post_id,
-        CASE WHEN k."type" = 'comment' THEN k.comment_id ELSE NULL END AS comment_id,
-        k."type",
-        k.sentence,
-        k.category,
-        k.sentiment_score,
-        CASE 
-            WHEN k."type" = 'post' THEN p.create_date
-            WHEN k."type" = 'comment' THEN cm.create_date
-        END AS create_date,
-        k.ingestion_date,
-        k.car_name,
-        c.pre_car_name,
-        c.release_date,
-        k.source,
-        w.age
-    FROM mart.tb_keywords AS k
-    JOIN mart.tb_car AS c ON k.car_name = c.car_name
-    JOIN mart.tb_web_source AS w ON k.source = w.source
-    JOIN mart.tb_posts AS p ON k.post_id = p.post_id
-    LEFT JOIN (
-        SELECT c.comment_id, c.create_date
-        FROM mart.tb_comments c
-    ) AS cm ON k.comment_id = cm.comment_id;
+REFRESH MATERIALIZED VIEW social_listening;
+REFRESH MATERIALIZED VIEW social_attention;
--- airflow/dags/sql/init_mart.sql
+++ airflow/dags/sql/init_mart.sql
@@ -76,3 +76,49 @@ CREATE TABLE IF NOT EXISTS mart.tb_keywords(
     FOREIGN KEY (car_name)              REFERENCES mart.tb_car          (car_name),
     FOREIGN KEY (source)                REFERENCES mart.tb_web_source   (source)
 );
+
+CREATE MATERIALIZED VIEW social_listening AS
+    SELECT
+        k.post_id,
+        CASE WHEN k."type" = 'comment' THEN k.comment_id ELSE NULL END AS comment_id,
+        k."type",
+        k.sentence,
+        k.category,
+        k.sentiment_score,
+        k.ingestion_date,
+        k.car_name,
+        c.pre_car_name,
+        c.release_date,
+        k.source,
+        w.age
+    FROM mart.tb_keywords AS k
+    JOIN mart.tb_car AS c ON k.car_name = c.car_name
+    JOIN mart.tb_web_source AS w ON k.source = w.source;
+
+CREATE MATERIALIZED VIEW social_attention AS
+    SELECT
+       p.post_id,
+       c.comment_id,
+        CASE
+            WHEN comment_id IS NULL THEN p.like_cnt
+            ELSE c.like_cnt
+        END AS like_cnt,
+        CASE
+            WHEN comment_id IS NULL THEN p.dislike_cnt
+            ELSE c.dislike_cnt
+        END AS dislike_cnt,
+        p.view_cnt,
+        p.comment_cnt,
+        CASE
+            WHEN comment_id IS NULL THEN CAST(p.ingestion_date)
+            ELSE CAST(c.ingestion_date)
+        END AS ingestion_date,
+        p.car_name,
+        car.pre_car_name,
+        car.release_date,
+        p.source,
+        w.age
+    FROM mart.tb_posts_metric AS p
+        LEFT JOIN mart.tb_comments_metric AS c ON p.post_id = c.post_id
+        JOIN mart.tb_car AS car ON p.car_name = car.car_name
+        JOIN mart.tb_web_source AS w ON p.source = w.source
